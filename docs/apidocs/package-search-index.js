a0c88073d8e562fee44cdc4b8bb9c3e6d9091f18
Mike Cowan <mike.c.cowan@gmail.com>
2023-05-31 10:43:11 +0100

    Update apidocs

--- docs/apidocs/package-search-index.js
+++ docs/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.blt.util.stream"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.blt.util"},{"l":"io.blt.util.stream"}];updateSearchResults();
